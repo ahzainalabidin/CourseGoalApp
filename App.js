49a320566250708e63018e9e81bd1d4419dd16c8
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-06-18 16:08:25 +0800

    Add text input and button with styling

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+
+      <View>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
+      </View>
+
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  appContainer: {
+    padding: 50
+  }
 });
