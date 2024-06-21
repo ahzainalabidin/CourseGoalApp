13d399dc327e7f74abd9400fdf9fe32016130166
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-21 21:14:48 +0800

    Dynamically display list of goals via state

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
 
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredGoalText) {
     setEnteredGoalText(enteredGoalText);
   };
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      enteredGoalText
+    ]);
   };
 
   return (
@@ -22,7 +26,7 @@ export default function App() {
       </View>
 
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
 
     </View>
