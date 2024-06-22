754fc450068e76605828ae05e4443d88f56a76da
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-22 20:42:29 +0800

    Create ripple effect when item hold pressed

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const GoalItem = ({ id, text, onDeleteItem }) => {
     return (
-        <Pressable onPress={onDeleteItem.bind(this, id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable
+                onPress={onDeleteItem.bind(this, id)}
+                android_ripple={{ color: '#dddddd' }}
+                style={({ pressed }) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>{text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 }
 
@@ -15,11 +19,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
     },
     goalText: {
-        color: 'white'
+        color: 'white',
+        padding: 8
+    },
+    pressedItem: {
+        opacity: 0.5
     }
 });
